83bf2c819736ca9cf115b8610eec4bc54501b674
voeux-blamer <voeuxrochefort@gmail.com>
2021-08-12 19:27:47 +0700

    add button export excel

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -28,6 +28,10 @@
             <a href="/exportpdf" class="btn btn-info ">Export PDF</a>  
           </div>
 
+          <div class="col-auto">
+            <a href="/exportexcel" class="btn btn-success ">Export Excel</a>  
+          </div>
+
           
         </div>
         <div class="row">
