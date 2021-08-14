9e1e741cd9c4b6646cb31cb05ebb5fed1d8ab2e0
voeux-blamer <voeuxrochefort@gmail.com>
2021-08-14 10:39:17 +0700

    add modal for import excel

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -32,7 +32,40 @@
             <a href="/exportexcel" class="btn btn-success ">Export Excel</a>  
           </div>
 
+          <div class="col-auto">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Import data
+            </button> 
+          </div>
           
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="/importexcel" method="POST" enctype="multipart/form-data">
+                  @csrf
+                <div class="modal-body">
+                  <div class="form-group">
+                    <input type="file" name="file" required>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </form>
+
+            </div>
+          </div>
+        <!-- end button modal !-->
+
         </div>
         <div class="row">
           {{-- @if ($message = Session::get('success'))
